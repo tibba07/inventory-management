cd6b71f9632416bdab03fc15c356c4cbd90fee62
FREDOLIN TIBBA <fredolintibba@gmail.com>
2023-04-29 09:28:55 +0530

    client file added

---
+++ client/src/components/homeComponents/ContactInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const ContactInfo = () => {
+  return (
+    <div className="contactInfo container">
+      <div className="row">
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-phone-alt"></i>
+            </div>
+            <h5>Call Us 24x7</h5>
+            <p>+91 9865786842</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-map-marker-alt"></i>
+            </div>
+            <h5>Location</h5>
+            <p>Valarai Gate,Tiruchengode-637 211</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-fax"></i>
+            </div>
+            <h5>Mail</h5>
+            <p>murugasen@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
